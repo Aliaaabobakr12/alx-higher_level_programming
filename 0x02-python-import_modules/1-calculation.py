7160c6f873237dd7e016e693e76e5ee43be166ee
Aliaaabobakr12 <aliaaabobakr12@gmail.com>
2023-08-10 22:23:46 +0000

    udpated

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1
+    
+    a = 10
+    b = 5
+    print(f"{a} + {b} = {calculator_1.add(a, b)}\n{a} - {b} = {calculator_1.sub(a, b)}\n{a} * {b} = {calculator_1.mul(a, b)}\n{a} / {b} = {calculator_1.div(a, b)}\n")
