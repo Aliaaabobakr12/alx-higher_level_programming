40b3784621cb6ee37fe32dc20d24e5c9263d6b9a
Aliaaabobakr12 <aliaaabobakr12@gmail.com>
2023-08-13 19:34:39 +0000

    m

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) <= 0:
-        return None
+    if len(sentence) == 0:
+        return (0, None)
     else:
-        length = len(sentence)
-        first = sentence[0]
-        return length, first
+        return (len(sentence), sentence[0])
